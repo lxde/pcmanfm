7efcd50057b37ee50855a2d2c06b47923ff8849f
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2010-01-26 01:34:01 +0800

    Add missing file.

---
+++ src/desktop-ui.c
@@ -0,0 +1,83 @@
+/*
+ *      desktop-ui.c
+ *      
+ *      Copyright 2010 PCMan <pcman.tw@gmail.com>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+
+/* this file is included by main-win.c */
+
+static const char desktop_menu_xml[] = 
+"<popup>"
+  "<menu action='CreateNew'>"
+    "<menuitem action='NewFolder'/>"
+    "<menuitem action='NewBlank'/>"
+  "</menu>"
+  "<separator/>"
+  "<menuitem action='Paste'/>"
+  "<menu action='Sort'>"
+    "<menuitem action='Desc'/>"
+    "<menuitem action='Asc'/>"
+    "<separator/>"
+    "<menuitem action='ByName'/>"
+    "<menuitem action='ByMTime'/>"
+  "</menu>"
+  "<separator/>"
+  "<menuitem action='Prop'/>"
+"</popup>";
+
+static GtkActionEntry desktop_actions[]=
+{
+    {"Paste", GTK_STOCK_PASTE, NULL, NULL, NULL, G_CALLBACK(on_paste)},
+    {"SelAll", GTK_STOCK_SELECT_ALL, NULL, NULL, NULL, G_CALLBACK(on_select_all)},
+    {"InvSel", NULL, N_("Invert Selection"), NULL, NULL, G_CALLBACK(on_invert_select)},
+    {"Sort", NULL, N_("_Sort Files"), NULL, NULL, NULL},
+    {"CreateNew", GTK_STOCK_NEW, NULL, NULL, NULL, NULL},
+    {"NewFolder", "folder", N_("Folder"), NULL, NULL, G_CALLBACK(on_create_new)},
+    {"NewBlank", "text-x-generic", N_("Blank FIle"), NULL, NULL, G_CALLBACK(on_create_new)},
+    {"Prop", GTK_STOCK_PROPERTIES, NULL, NULL, NULL, G_CALLBACK(fm_desktop_preference)}
+};
+
+static GtkRadioActionEntry desktop_sort_type_actions[]=
+{
+    {"Asc", GTK_STOCK_SORT_ASCENDING, NULL, NULL, NULL, GTK_SORT_ASCENDING},
+    {"Desc", GTK_STOCK_SORT_DESCENDING, NULL, NULL, NULL, GTK_SORT_DESCENDING},
+};
+
+static GtkRadioActionEntry desktop_sort_by_actions[]=
+{
+    {"ByName", NULL, N_("By _Name"), NULL, NULL, COL_FILE_NAME},
+    {"ByMTime", NULL, N_("By _Modification Time"), NULL, NULL, COL_FILE_MTIME}
+};
+
+static const char folder_menu_xml[]=
+"<popup>"
+  "<placeholder name='ph1'>"
+    "<menuitem action='NewTab'/>"
+    "<menuitem action='NewWin'/>"
+    /* "<menuitem action='Search'/>" */
+  "</placeholder>"
+"</popup>";
+
+/* Action entries for pupup menus */
+static GtkActionEntry folder_menu_actions[]=
+{
+    {"NewTab", GTK_STOCK_NEW, N_("Open in New Tab"), NULL, NULL, G_CALLBACK(on_open_in_new_tab)},
+    {"NewWin", GTK_STOCK_NEW, N_("Open in New Window"), NULL, NULL, G_CALLBACK(on_open_in_new_win)},
+    {"Search", GTK_STOCK_FIND, NULL, NULL, NULL, NULL}
+};
+
