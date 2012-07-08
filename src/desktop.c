984ccbd9d4956de7ef20e0a0f716dad7b7f8a806
Andriy Grytsenko <andrej@rep.kiev.ua>
2012-07-08 21:24:13 +0300

    Renumber into 1.0-rc1.

--- src/desktop.c
+++ src/desktop.c
@@ -2,6 +2,7 @@
  *      desktop.c
  *
  *      Copyright 2010 - 2012 Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
+ *      Copyright 2012 Andriy Grytsenko (LStranger) <andrej@rep.kiev.ua>
  *
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
