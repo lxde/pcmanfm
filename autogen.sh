7cd0d7986b1a90071031b5fd488b828b2db6ee1c
Andriy Grytsenko <andrej@rep.kiev.ua>
2012-10-30 01:01:35 +0200

    Remove libtoolize from autogen.sh: the pcmanfm doesn't use libtool.

--- autogen.sh
+++ autogen.sh
@@ -23,7 +23,6 @@ fi
 
 ${ACLOCAL:-aclocal$AM_VERSION} ${ACLOCAL_ARG}
 ${AUTOHEADER:-autoheader$AC_VERSION} --force
-AUTOMAKE=$AUTOMAKE libtoolize -c --automake --force
 AUTOMAKE=$AUTOMAKE intltoolize -c --automake --force
 $AUTOMAKE --add-missing --copy --include-deps
 ${AUTOCONF:-autoconf$AC_VERSION}
